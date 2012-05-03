693dac9da668b32bf0d209e865ad6e923b613e2d
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2012-05-03 19:33:50 -0300

    Adicionado exemplo com parametro.

--- main.c
+++ main.c
@@ -11,11 +11,22 @@ static foreign_t f0(void)
 	/* return FALSE; */
 }
 
+static foreign_t f1(term_t t0)
+{
+	int i;
+
+	PL_get_integer(t0, &i);
+	
+	printf("param1: %d\n", i);
+	return TRUE;
+}
+
 int main(int argc, char *argv[])
 {
 	if (!PL_initialise(1, argv)) PL_halt(1);
 
 	PL_register_foreign("f0", 0, f0, 1);
+	PL_register_foreign("f1", 1, f1, 1);
 	PL_halt(PL_toplevel() ? 0 : 1);
 	return 0;
 }
