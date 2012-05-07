0515f4ba4c70666ee0af5f23b9f9c9b97384f88a
Julio Ribeiro <julio@juliords.com>
2012-05-07 15:43:02 -0300

    First version of strategy.pl

---
+++ strategy.pl
@@ -0,0 +1,67 @@
+
+% ----------------------------------- %
+
+:- 	dynamic has_zombie/3, 
+	curr_position/2, 
+	has_antidote/2, 
+	has_ammo/2, 
+	safe_tile/2.
+
+% ----------------------------------- %
+
+load_position :-
+	consult_position(X,Y),
+	retract(curr_position(_,_)),
+	assert(curr_position(X,Y),
+	assert(safe_tile(X,Y)).
+
+% ----------------------------------- %
+
+load_sense_zombies :-
+	sense_zombies(N),
+	true.
+	
+load_sense_station :-
+	sense_station(),
+	true.
+
+load_sense_hospital :-
+	sense_hospital(),
+	true.
+
+load_sense_antidote :-
+	sense_antidote(),
+	curr_position(X,Y),
+	has_antidote(X,Y).
+
+load_sense_ammo :-
+	sense_ammo(N),
+	curr_position(X,Y),
+	has_ammo(coord(X,Y), N).
+
+load_sense_hit :-
+	true.
+
+init_sensors :-
+	load_sense_zombie,
+	load_sense_station,
+	load_sense_hospital,
+	load_sense_antidote,
+	load_sense_ammo,
+	load_sense_hit.
+
+% ----------------------------------- %
+
+logic :-
+	true.
+
+% ----------------------------------- %
+
+strategy :-
+	load_position,
+	init_sensors,
+	logic.
+
+% ----------------------------------- %
+
+
